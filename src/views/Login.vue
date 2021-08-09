c6534e6072d73d73b7364c2bfea198de09a2200d
Ryan Searls <Ryan.searls@gmail.com>
2021-08-09 12:29:46 -0500

    added login page

---
+++ src/views/Login.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="login">
+    <form v-on:submit.prevent="submit()">
+      <h1>login</h1>
+      <ul>
+        <li v-for="error in errors" v-bind:key="error">{{ error }}</l1>
+      </ul>
+      <div>
+        <label>Email:</label>
+        <input type="email" v-model="newSessionsParams.email" />
+      </div>
+      <div>
+        <label>Password:</label>
+        <input type="password" v-model="newSessionsParams.password" />
+      </div>
+      <input type="submit" value="Submit" />
+    <form>
+  </div>      
+</template>
+
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      newSessionParams: {},
+      errors: [],
+    };
+  },
+  methods: {
+    submit: function () {
+      axios
+        .post("/sessions", this.newSessionParams)
+        .then((response) => {
+          axios.defaults.headers.common["Authorization"] = "Bearer " + response.data.jwt;
+          localStorage.setItem("jwt", response.data.jwt);
+          localStorage.setItem("user_id", response.data.user_id);
+          this.$router.push("/");
+        })
+        .catch((error) => {
+          console.log(error.response);
+          this.errors = ["Invalid email or password."];
+          this.email = "";
+          this.password = "";
+        });
+    },
+  },
+};
+</script>
+  
+  
+
+
+
+
+
+
+
+}
+
+
+
---
+++ src/views/RestaurantsIndex.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="restaurants-index">
+    <div v-for="restaurant in restaurants" :key="restaurant.id">
+
+
+</template>    
