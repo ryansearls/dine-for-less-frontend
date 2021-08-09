e44f21646fb5abce307eadb15aa359acf8e124cd
Ryan Searls <Ryan.searls@gmail.com>
2021-08-09 13:18:45 -0500

    added logout page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+// import RestaurantsIndex from "..views/RestaurantsIndex.vue";
 
 Vue.use(VueRouter);
 
@@ -18,6 +19,13 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
+  // {
+  //   path: "/restaurants",
+  //   name: "restaurants-index",
+  //   component: RestaurantsIndex,
+  // },
+
+
 ];
 
 const router = new VueRouter({
--- src/views/RestaurantsIndex.vue
+++ src/views/RestaurantsIndex.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="restaurants-index">
-    <div v-for="restaurant in restaurants" :key="restaurant.id">
-
-
-</template>    
+  <div class="restaurants-index"
+    <div v-for="restaurant in restaurants" :key="restaurant.id"
+      <router-link v-bind:to="`/restaurants/#{restaurant.id}`">
+        <h2>Name: {{ restaurant.name }}</h2>
+      <router-link>
+    </div>
+  </div>      
+</template>      
