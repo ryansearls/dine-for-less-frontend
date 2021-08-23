59d6068d19699b0d981445d2db443af6e53b705b
Ryan Searls <Ryan.searls@gmail.com>
2021-08-23 14:34:03 -0500

    got mapbox uploaded woot woot

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,12 +2,19 @@
   <div class="home">
     <h1>{{ message }}</h1>
     <h2>{{ message2 }}</h2>
+    <div id="map"></div>
   </div>
 </template>
 
-<style></style>
+<style>
+#map {
+  height: 350px;
+  width: auto;
+}
+</style>
 
 <script>
+/* global mapboxgl */
 export default {
   data: function () {
     return {
@@ -15,7 +22,38 @@ export default {
       message2: "Where stomachs and wallets stay full",
     };
   },
+  mounted: function () {
+    this.setUpMap();
+  },
   created: function () {},
-  methods: {},
+  methods: {
+    setUpMap: function () {
+      mapboxgl.accessToken =
+        "pk.eyJ1IjoicnNlYXJscyIsImEiOiJja3JwOXdybmYyNjJiMnpxcmg4bWFhNGYzIn0.HTFQvxEQZzDP5lngLTB6gQ";
+      var map = new mapboxgl.Map({
+        container: "map", // container id
+        style: "mapbox://styles/rsearls/cksozuxqa0u9d17o38dyx0o8a", // style URL
+        center: [-104.9903, 39.7392], // starting position [lng, lat]
+        zoom: 11, // starting zoom
+      });
+
+      map.on("click", function (e) {
+        // If the user clicked on one of your markers, get its information.
+        var features = map.queryRenderedFeatures(e.point, {
+          layers: ["denver-restaurants"], // replace with your layer name
+        });
+        if (!features.length) {
+          return;
+        }
+        var feature = features[0];
+        var popup = new mapboxgl.Popup({ offset: [0, -15] })
+          .setLngLat(feature.geometry.coordinates)
+          .setHTML("<h3>" + feature.properties.title + "</h3>" + "<p>" + feature.properties.description + "</p>")
+          .addTo(map);
+      });
+
+      console.log(map);
+    },
+  },
 };
 </script>
